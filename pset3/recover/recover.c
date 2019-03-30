5bc570c73cd7dbbb35b50b160b3f773e898bb0ae
Reginaldo Morikawa <morikawa77@gmail.com>
2019-03-30 01:13:42 -0300

    pset3 riseze and recover

--- pset3/recover/recover.c
+++ pset3/recover/recover.c
@@ -3,5 +3,60 @@
 
 int main(int argc, char *argv[])
 {
+    if (argc != 2)
+    {
+        printf("Usage: ./recover image\n");
+        return 1;
+    }
+    // open memory card file
+    FILE *inptr = fopen(argv[1], "r");
 
+    if (inptr == NULL)
+    {
+        fclose(inptr);
+        fprintf(stderr, "Could not open argv[1]");
+        return 1;
+    }
+
+    // Allocate memory for 512 byte chunks
+    unsigned char buffer[512];
+
+    // How many jpgs found
+    int counter = 0;
+
+    // Current filename and img
+    char title[10];
+    FILE *img;
+
+    // Read until end of card
+    while (fread(&buffer, 512, 1, inptr))
+    {
+        // start of new jpg?
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 &&
+            buffer[2] == 0xff && buffer[3] >> 4 == 0xe)
+        {
+            // Found jpg
+            counter++;
+
+            // Close prev file (if any)
+            if (counter > 1)
+            {
+                fclose(img);
+            }
+
+            // Open new file and write first buffer
+            sprintf(title, "%03d.jpg", counter - 1);
+            img = fopen(title, "a");
+        }
+
+        // If currently writing to file, write buffer
+        if (counter > 0)
+        {
+            fwrite(&buffer, 512, 1, img);
+        }
+    }
+
+    // close any remaining files
+    fclose(img);
+    fclose(inptr);
 }
